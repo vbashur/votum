55322957ff9d5350ff7df5c07555ddbead46284d
vbashur <vbashur@localhost>
2015-04-23 19:02:55 +0900

    merge

---
+++ dataaccess/hibernate-2-start/src/main/java/rewards/internal/reward/RewardRepository.java
@@ -0,0 +1,23 @@
+package rewards.internal.reward;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import rewards.AccountContribution;
+import rewards.Dining;
+import rewards.RewardConfirmation;
+
+/**
+ * Handles creating records of reward transactions to track contributions made to accounts for dining at restaurants.
+ */
+public interface RewardRepository {
+
+	/**
+	 * Create a record of a reward that will track a contribution made to an account for dining.
+	 * @param contribution the account contribution that was made
+	 * @param dining the dining event that resulted in the account contribution
+	 * @return a reward confirmation object that can be used for reporting and to lookup the reward details at a later
+	 * date
+	 */
+	@Transactional
+	public RewardConfirmation confirmReward(AccountContribution contribution, Dining dining);
+}
