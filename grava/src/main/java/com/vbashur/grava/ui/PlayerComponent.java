be889841483e081de12c520da1401419fd31f5e2
vbashur <vbashur@localhost>
2015-04-30 11:39:56 +0900

    UI modification

--- grava/src/main/java/com/vbashur/grava/ui/PlayerComponent.java
+++ grava/src/main/java/com/vbashur/grava/ui/PlayerComponent.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.vaadin.server.ClassResource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vbashur.grava.Const;
 import com.vbashur.grava.Player;
@@ -31,16 +33,12 @@ public class PlayerComponent extends CustomComponent {
 	public PlayerComponent(Player p, GravaArbiter arb) {
 		player = p;
 		arbiter = arb;
-	}
-
-	private List<Entry<Button, Integer>> buttonIndexPair;
-
-	@Override
-	public void attach() {
 		buildLayout();
 		buildPits();
 		setCompositionRoot(grid);
 	}
+
+	private List<Entry<Button, Integer>> buttonIndexPair;
 
 	private void buildLayout() {
 		grid = new GridLayout(Const.DEFAULT_PIT_NUM + 2, 2);
@@ -55,10 +53,13 @@ public class PlayerComponent extends CustomComponent {
 		if (player == Player.SPOUNGE_BOB) {
 			grid.addComponent(nameLabel, 1, 0, Const.DEFAULT_PIT_NUM, 0);
 			for (int iter = 0; iter <= Const.DEFAULT_PIT_NUM; ++iter) {
-				final int buttonIndex = iter;
+				final int buttonIndex = Const.DEFAULT_PIT_NUM - iter  + 1;
 				Button b = new Button();
 				if (iter == 0) {
 					b.setEnabled(false);
+					b.setIcon(new ClassResource("/static/img/ico_valid.png"), "grava");
+					b.setWidth(Const.DEFAULT_WIDTH, Unit.PIXELS);
+					buttonIndexPair.add(new java.util.AbstractMap.SimpleEntry<>(b, 0));
 				} else {
 					b.addClickListener(new ClickListener() {
 						
@@ -67,19 +68,27 @@ public class PlayerComponent extends CustomComponent {
 							arbiter.makeTurn(player, buttonIndex);							
 						}
 					});					
+					buttonIndexPair.add(new java.util.AbstractMap.SimpleEntry<>(b, buttonIndex));
 				}
 				grid.addComponent(b, iter, 1);
-				buttonIndexPair.add(new java.util.AbstractMap.SimpleEntry<>(b, iter));
+				
 			}
-			grid.addComponent(new Label("^^"), Const.DEFAULT_PIT_NUM + 1, 1); // placeholder
+			Image bobImage = new Image();
+			bobImage.setSource(new ClassResource("/static/img/spoungebob.png"));
+			grid.addComponent(bobImage, Const.DEFAULT_PIT_NUM + 1, 0,Const.DEFAULT_PIT_NUM + 1, 1);
 
 		} else {
-			grid.addComponent(new Label(":)"), 0, 0); // placeholder
+			Image patrickImage = new Image();
+			patrickImage.setSource(new ClassResource("/static/img/patrick.png"));
+			grid.addComponent(patrickImage, 0, 0, 0, 1);
 			for (int iter = 1; iter <= Const.DEFAULT_PIT_NUM + 1; ++iter) {
 				Button b = new Button();
-				final int buttonIndex = Const.DEFAULT_PIT_NUM + 1 - iter;
+				final int buttonIndex = iter;
 				if (iter == Const.DEFAULT_PIT_NUM + 1) {
 					b.setEnabled(false);
+					b.setIcon(new ClassResource("/static/img/ico_valid.png"), "grava");
+					b.setWidth(Const.DEFAULT_WIDTH, Unit.PIXELS);
+					buttonIndexPair.add(new java.util.AbstractMap.SimpleEntry<>(b, 0));
 				} else {
 					b.addClickListener(new ClickListener() {
 						
@@ -87,10 +96,11 @@ public class PlayerComponent extends CustomComponent {
 						public void buttonClick(ClickEvent event) {
 							arbiter.makeTurn(player, buttonIndex);							
 						}
-					});	
+					});
+					buttonIndexPair.add(new java.util.AbstractMap.SimpleEntry<>(b, buttonIndex));
 				}
 				grid.addComponent(b, iter, 0);
-				buttonIndexPair.add(new java.util.AbstractMap.SimpleEntry<>(b, buttonIndex));
+				
 			}
 
 			grid.addComponent(nameLabel, 1, 1, Const.DEFAULT_PIT_NUM, 1);
