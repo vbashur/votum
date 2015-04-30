f8306e9fc31499b83f064188b44c693a0da6a135
vbashur <vbashur@localhost>
2015-04-30 10:14:42 +0900

    grava event handling improvement

--- grava/src/main/java/com/vbashur/grava/game/PlayerInfo.java
+++ grava/src/main/java/com/vbashur/grava/game/PlayerInfo.java
@@ -1,28 +1,59 @@
 package com.vbashur.grava.game;
 
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Service;
+import com.vbashur.grava.Player;
+import com.vbashur.grava.ui.PlayerComponent;
 
-import com.vbashur.grava.game.GravaEvent.OnCapturingStone;
 
-@Service
-public class PlayerInfo implements ApplicationListener<GravaEvent.OnCapturingStone> {
+public class PlayerInfo {
 	
-	@Autowired
-	private GravaArbiter gravaArbiter;
-
-	private GameBoard gameBoardA;
+	private Player player;
 	
-	private GameBoard gameBoardB;
+	private GameBoard gameBoard;
+	
+	private PlayerComponent playerComponent;
+
+	
+	public PlayerInfo(Player p, GravaArbiter arbiter) {
+		this.player = p;
+		this.playerComponent = new PlayerComponent(p, arbiter);
+		this.gameBoard = new GameBoard(p, arbiter);
+	}
+	
+	public PlayerComponent getPlayerComponent() {
+		return this.playerComponent;
+	}
+	
+	public Player getPlayer() {
+		return this.player;
+	}
+	
+	public void makeTurn(Integer startIndex) {
+		this.gameBoard.makeTurn(startIndex);
+	}
+	
+	public Integer giveStones(Integer pitIndex) {
+		return this.gameBoard.giveStones(pitIndex);
+	}
+	
+	public void grabStones(Integer stonesNum) {
+		this.gameBoard.addGravaStones(stonesNum);
+	}
+	
+	public void refreshComponent() {
+		Map<Integer, Integer> stateMap = gameBoard.getPits();
+		playerComponent.updateState(stateMap);
+	}
 	
 	@PostConstruct
 	public void init() {
-		gameBoardA = new GameBoard(gravaArbiter);
-		gameBoardB = new GameBoard(gravaArbiter);
+	
 	}
+	
+	
 	
 //	
 //	public void grabOppositeStones(GameBoard target, int index) {
@@ -36,18 +67,19 @@ public class PlayerInfo implements ApplicationListener<GravaEvent.OnCapturingSto
 //	}
 
 
-	@Override
-	public void onApplicationEvent(OnCapturingStone event) {
-		GameBoard target = (GameBoard)event.getSource();
-		int index = event.getIndex();
-		if (target.equals(gameBoardA)) {
-			Integer grabbedStones = gameBoardB.giveStones(index);
-			gameBoardA.addGravaStones(grabbedStones);
-		} else {
-			Integer grabbedStones = gameBoardA.giveStones(index);
-			gameBoardB.addGravaStones(grabbedStones);						
-		}		
-	}
+//	@Override
+//	public void onApplicationEvent(OnCapturingStone event) {
+//		GameBoard target = (GameBoard)event.getSource();
+//		int index = event.getIndex();
+//		if (target.equals(gameBoardA)) {
+//			Integer grabbedStones = gameBoardB.giveStones(index);
+//			gameBoardA.addGravaStones(grabbedStones);
+//		} else {
+//			Integer grabbedStones = gameBoardA.giveStones(index);
+//			gameBoardB.addGravaStones(grabbedStones);						
+//		}		
+//	}
+
 	
 	
 }
